99310f90174c91a5c7a72a1e7dac7add65ab7b13
Cael <MR-Addict@qq.com>
2025-06-17 00:49:04 +0800

    ci: use vite as bundler

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,31 @@
-import { LitElement, css, html } from "lit";
+// @ts-expect-error
+import ribbonStyles from "github-fork-ribbon-css/gh-fork-ribbon.css?inline";
+
+import { LitElement, html, unsafeCSS } from "lit";
 import { customElement, property } from "lit/decorators.js";
 
 @customElement("wc-fork-me-on-github")
 export class WcForkMeOnGithub extends LitElement {
-  static styles = css`
-    :host {
-      color: red;
-    }
-  `;
+  static styles = unsafeCSS(ribbonStyles);
 
-  @property()
-  name?: string = "World";
+  @property({ type: String })
+  ribbon: string = "Fork me on Github";
+
+  @property({ type: String })
+  username: string = "";
+
+  @property({ type: String })
+  position: "right-top" | "left-top" | "left-bottom" | "right-bottom" = "right-top";
 
   render() {
-    return html`<p>Hello, ${this.name}!</p>`;
+    return html`<a
+      target="_blank"
+      title="${this.ribbon}"
+      data-ribbon="${this.ribbon}"
+      class="github-fork-ribbon fixed ${this.position}"
+      href="https://github.com/${this.username}"
+    >
+      Fork me on Github
+    </a>`;
   }
 }
